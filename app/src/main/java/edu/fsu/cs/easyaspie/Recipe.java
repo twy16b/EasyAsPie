3259d2a03a803a0f7e55aafa0eea2b67a9cfc5ad
Bianca Regulski <biancaregulski@yahoo.com>
2019-11-26 16:39:53 -0500

    Add RecipesAdapter and begin AddRecipeActivity

---
+++ app/src/main/java/edu/fsu/cs/easyaspie/Recipe.java
@@ -0,0 +1,13 @@
+package edu.fsu.cs.easyaspie;
+
+public class Recipe {
+    private String mName;
+
+    public Recipe(String name) {
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
--- app/src/main/java/edu/fsu/cs/easyaspie/RecipesActivity.java
+++ app/src/main/java/edu/fsu/cs/easyaspie/RecipesActivity.java
@@ -1,5 +1,6 @@
 package edu.fsu.cs.easyaspie;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,24 +8,51 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
-public class RecipesActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class RecipesActivity extends AppCompatActivity implements RecipesAdapter.ItemClickListener {
+
+    RecipesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Recipes");
         setContentView(R.layout.activity_recipes);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent myIntent = new Intent(RecipesActivity.this, AddRecipeActivity.class);
+                RecipesActivity.this.startActivity(myIntent);
+                /*Snackbar.make(view, "Add button clicked", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
             }
         });
+        ArrayList<String> foodNames = new ArrayList<>();
+        foodNames.add("Spaghetti");
+        foodNames.add("Chicken");
+        foodNames.add("Salad");
+        foodNames.add("Pie");
+        foodNames.add("Cake");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recipes_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecipesAdapter(this, foodNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 
+    @Override
+    public void onItemClick(View view, int position) {
+        Snackbar.make(view, "You clicked " + adapter.getItem(position) + " on row number " + position, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
 }
