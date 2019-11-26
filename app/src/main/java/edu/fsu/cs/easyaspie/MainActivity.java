c4a46be1049172286186ea948edd1d697297c94f
Bianca Regulski <biancaregulski@yahoo.com>
2019-11-25 22:13:10 -0500

    Create main menu and recipe activity

--- app/src/main/java/edu/fsu/cs/easyaspie/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/easyaspie/MainActivity.java
@@ -2,7 +2,9 @@ package edu.fsu.cs.easyaspie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button_recipes:
+                Intent myIntent = new Intent(MainActivity.this, RecipesActivity.class);
+                MainActivity.this.startActivity(myIntent);
+                break;
+            case R.id.button_grocery:
+                break;
+            case R.id.button_times:
+                break;
+        }
     }
 }
