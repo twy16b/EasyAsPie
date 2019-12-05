0633dfde8572cac92c556ba3ecb894790e91cc1c
Bianca Regulski <biancaregulski@yahoo.com>
2019-12-04 19:36:20 -0500

    Implement swiping between fragments

--- app/src/main/java/edu/fsu/cs/easyaspie/TimerStepFragment.java
+++ app/src/main/java/edu/fsu/cs/easyaspie/TimerStepFragment.java
@@ -4,14 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class TimerStepFragment extends Fragment {
+    public TextView stepText;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // TODO: load step and timer from database and start timer on button click
-        return inflater.inflate(R.layout.fragment_timer_step, container, false);
+        final View v = inflater.inflate(R.layout.fragment_timer_step, container, false);
+        stepText = v.findViewById(R.id.text_step);
+        stepText.setText(getArguments().getString("newStep"));
+        return v;
     }
 }
